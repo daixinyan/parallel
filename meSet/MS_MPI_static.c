00edf7619fd4ec61f23944c41cc9813564f947dd
daixinyan <381675152@qq.com>
2016-11-18 11:54:45 +0800

    search size

--- meSet/MS_MPI_static.c
+++ meSet/MS_MPI_static.c
@@ -113,6 +113,7 @@ int main(int argc,char *argv[])
         MPI_Init(&argc, &argv);
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
         MPI_Comm_size(MPI_COMM_WORLD, &size);
+				printf("%d\n", size);
 
     total_start_time = MPI_Wtime();
 
