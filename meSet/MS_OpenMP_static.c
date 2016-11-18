4e6a1e9ed10b8c81c9d743484f7550d647f06436
daixinyan <381675152@qq.com>
2016-11-18 10:36:23 +0800

    add time.

--- meSet/MS_OpenMP_static.c
+++ meSet/MS_OpenMP_static.c
@@ -65,7 +65,7 @@ int main(int argc,char *argv[])
 	 my_excute();
    clock_t end_clock = clock();
   			 time_t  end_time = time(NULL);
-   printf("CLOCK:  %f\n",(end_clock-start_clock)/CLOCKS_PER_SEC);
+   printf("CLOCK:  %ld\n",(end_clock-start_clock)/CLOCKS_PER_SEC);
    printf("TIME:  %f\n",difftime(end_time, start_time) );
 
    my_draw();
