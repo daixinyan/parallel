4f918c27a78824d41fea0810c2c3e637bae3b8a6
daixinyan <381675152@qq.com>
2016-12-06 15:39:40 +0800

    remove useless.

--- sssp/SSSP_MPI_sync.c
+++ sssp/SSSP_MPI_sync.c
@@ -3,28 +3,7 @@
 
 int main(int argc,char *argv[])
 {
-
-    /**record time**/
-    double total_start_time;
-    double total_end_time;
-
-    /**init mpi**/
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    total_start_time = MPI_Wtime();
-
-
-    total_end_time = MPI_Wtime();
-    total_time = total_end_time - total_start_time;
-    compution_time = total_time - communication_time;
-
-		if(PRINT_TIME)
-    {
-        printf( "rank: %d\n total_time: %f\n communication_time: %f\n compution_time: %f\n",
-                 rank, total_time, communication_time, compution_time );
-    }
-
-    MPI_Finalize();
+    my_init(argc, argv);
+    my_finalize();
+    return 0;
 }
--- sssp/include.h
+++ sssp/include.h
@@ -9,8 +9,30 @@
 #define IF_PRINT   0
 #define CENTER_NODE 0
 
+
+extern int   vertexes_number;
+extern int   edges_half_number;
+
+extern int   threads_number;
+extern char* input_file_name;
+extern char* output_file_name;
+extern int   source_vertex;
+
+extern int** graph_weight;
+
+/**record time**/
+extern double total_start_time;
+extern double total_end_time;
+/**start struct time**/
+extern double communication_time;
+extern double compution_time;
+extern double total_time0;
+/**end struct time**/
+
+
 /**function for init variables, init MPI.**/
-void  my_init(int argc,char *argv[]);
+void my_init(int argc,char *argv[]);
+void my_finalize();
 
 /**@see MPI_Sendrecv, add comsumption time to (double)communication_time**/
 void mySendrecv( const void *sendbuf, int sendcount, MPI_Datatype sendtype,int dest, int sendtag,
@@ -21,5 +43,3 @@ void myRecv(void *buf, int count, MPI_Datatype type,int source, int tag,MPI_Comm
 
 /**@see MPI_Send, add comsumption time to (double)communication_time**/
 void mySend(const void *buf, int count, MPI_Datatype type,int dest, int tag,MPI_Comm comm, MPI_Status *status );
-
-void read_graph();
