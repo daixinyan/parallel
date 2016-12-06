6319f68c8f14da00cc53a19b7a79c4d813eed41f
daixinyan <381675152@qq.com>
2016-12-06 18:55:28 +0800

    print messages.

--- sssp/SSSP_common.c
+++ sssp/SSSP_common.c
@@ -107,10 +107,10 @@ void  my_init(int argc,char *argv[])
         threads_number = atoi(argv[1]);
         input_file_name = argv[2];
         output_file_name = argv[3];
-        source_vertex = atoi(argv[4]);
+
+        source_vertex = argc==3? 0:atoi(argv[4]);
     }
-    // readGraph();
-    printf("line!!%d\n",rank );
+    readGraph();
 }
 
 
