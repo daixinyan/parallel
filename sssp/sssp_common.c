731cbf6cd6731b57811614f28ec9c7d4906cb2ae
daixinyan <381675152@qq.com>
2016-12-06 15:46:48 +0800

    remove mutual reference.

--- sssp/sssp_common.c
+++ sssp/sssp_common.c
@@ -105,3 +105,41 @@ void  my_init(int argc,char *argv[])
     }
     readGraph();
 }
+
+
+void mySendrecv(const void *sendbuf, int sendcount, MPI_Datatype sendtype,int dest, int sendtag,
+  void *recvbuf, int recvcount, MPI_Datatype recvtype,int source, int recvtag,MPI_Comm comm, MPI_Status *status)
+{
+  double start_time;
+  double end_time;
+  start_time = MPI_Wtime();
+  MPI_Sendrecv(sendbuf, sendcount, sendtype,dest, sendtag,
+    recvbuf, recvcount, recvtype,source, recvtag, comm,  status);
+  end_time = MPI_Wtime();
+  communication_time += end_time-start_time;
+}
+
+
+
+void myRecv(void *buf, int count, MPI_Datatype type,int source, int tag,MPI_Comm comm, MPI_Status *status )
+{
+    double start_time;
+    double end_time;
+    start_time = MPI_Wtime();
+    MPI_Recv(buf,count,type,source,tag,comm,status);
+    end_time = MPI_Wtime();
+    communication_time += end_time-start_time;
+}
+
+
+
+
+void mySend(const void *buf, int count, MPI_Datatype type,int dest, int tag,MPI_Comm comm, MPI_Status *status )
+{
+    double start_time;
+    double end_time;
+    start_time = MPI_Wtime();
+    MPI_Send(buf,count,type,dest,tag,comm);
+    end_time = MPI_Wtime();
+    communication_time += end_time-start_time;
+}
