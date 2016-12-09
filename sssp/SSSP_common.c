9d3420d494c1fe4ebba7461fbbc192cf0efae1ca
daixinyan <381675152@qq.com>
2016-12-09 20:26:04 +0800

    debug print.

--- sssp/SSSP_common.c
+++ sssp/SSSP_common.c
@@ -102,6 +102,7 @@ void my_mpi_finalize()
         printf("rank: %d\n total_time: %f\n communication_time: %f\n compution_time: %f\n",
                rank, total_time, communication_time, compution_time );
     }
+    printf("rank: end!\n", rank);
     MPI_Finalize();
 
 }
