8c3cc5dfcb38ad8e4951895ebd876e735d97c961
daixinyan <381675152@qq.com>
2016-12-09 09:21:30 +0800

    add test.

--- sssp/test.c
+++ sssp/test.c
@@ -6,10 +6,12 @@ int main(int argc ,char * argv[])
 
 
 	MPI_Request req[2];
+	MPI_Request sec_req[2];
 
 	int s1,r1;
 
-  MPI_Status sta[2];
+	MPI_Status sta[2];
+  MPI_Status sec_sta[2];
 
 	int nprocs,rank;
 	MPI_Init(&argc,&argv);
@@ -21,13 +23,18 @@ int main(int argc ,char * argv[])
 	if(rank==0){
 		//MPI_Send(&s1,1,MPI_DOUBLE,2,1,MPI_COMM_WORLD);
 		MPI_Isend(&s1,1,MPI_DOUBLE,2,1,MPI_COMM_WORLD,req);
+		MPI_Isend(&s1,1,MPI_DOUBLE,2,1,MPI_COMM_WORLD,sec_req);
 		MPI_Wait(req,sta);
+		MPI_Wait(sec_req,sec_sta);
 	}
 
 	if(rank==2){
 		//MPI_Recv(&r1,1,MPI_DOUBLE,0,1,MPI_COMM_WORLD,sta);
 		MPI_Irecv(&r1,1,MPI_DOUBLE,0,1,MPI_COMM_WORLD,&req[1]);
 		MPI_Wait(&req[1],&sta[1]);
+
+		MPI_Irecv(&r1,1,MPI_DOUBLE,0,1,MPI_COMM_WORLD,&sec_req[1]);
+		MPI_Wait(&sec_req[1],&sec_sta[1]);
  		printf("%d\n",r1);
 	}
 	printf("rank %d process is running\n",rank);
