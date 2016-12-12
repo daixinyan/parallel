b7f61b7e3e503c81ad4069bb9d5b9f50aa85d7d6
daixinyan <381675152@qq.com>
2016-12-12 20:17:32 +0800

    pthread.

--- sssp/SSSP_Pthread.c
+++ sssp/SSSP_Pthread.c
@@ -2,25 +2,25 @@
 
 void floyd(){
     int i, k, j;
-    for(i = 0;k< vertexes_number; i++)
+    for(i = 0;i < vertexes_number; i++)
     {
       result_collect[i] = source_vertex;
     }
-    // for(k = 0;k < vertexes_number;k++)
-    //         for(j = 0;j < vertexes_number;j++){
-    //             if( graph_weight[source_vertex][k] + graph_weight[k][j] < graph_weight[source_vertex][j])
-    //             {
-    //                 graph_weight[source_vertex][j] = graph_weight[source_vertex][k] + graph_weight[k][j];
-    //                 result_collect[j] = k;
-    //             }
-    //         }
+    for(k = 0;k < vertexes_number;k++)
+            for(j = 0;j < vertexes_number;j++){
+                if( graph_weight[source_vertex][k] + graph_weight[k][j] < graph_weight[source_vertex][j])
+                {
+                    graph_weight[source_vertex][j] = graph_weight[source_vertex][k] + graph_weight[k][j];
+                    result_collect[j] = k;
+                }
+            }
 }
 
 
 int main(int argc,char *argv[])
 {
     my_init(argc, argv);
-    // floyd();
+    floyd();
     print_result(result_collect);
     my_global_free();
 
