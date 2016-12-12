8a6d866d08689521b9eae220bd7482fa10441178
daixinyan <381675152@qq.com>
2016-12-12 19:36:25 +0800

    pthread.

--- sssp/SSSP_Pthread.c
+++ sssp/SSSP_Pthread.c
@@ -11,8 +11,9 @@ int main(int argc,char *argv[])
 }
 
 void floyd(){
-    for(int k = 0;k < vertexes_number;k++)
-            for(int j = 0;j < vertexes_number;j++){
+    int k, j;
+    for(k = 0;k < vertexes_number;k++)
+            for(j = 0;j < vertexes_number;j++){
                 if( graph_weight[source_vertex][k] + graph_weight[k][j] < graph_weight[source_vertex][j])
                 {
                     graph_weight[source_vertex][j] = graph_weight[source_vertex][k] + graph_weight[k][j];
