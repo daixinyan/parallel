7dca3cbfe63b80b98f121b24e121f5deb1f46b4c
darxan <381675152@qq.com>
2017-07-01 23:23:47 +0800

    fix bugs.

--- prime/main.cpp
+++ prime/main.cpp
@@ -9,21 +9,24 @@
 using namespace std;
 
 //static int threadNum = 1;
-static long MAX_NUM = 10 * 1000  ;
+static long MAX_NUM = 200 * 1000  ;
 
 
 int main(int argc, char ** args) {
 
 
     int i;
-    #pragma omp parallel for  private(i) schedule(static,1)  num_threads(parameters.number_of_threads)
-    for(long i=3; i<=MAX_NUM; i += 2 ) {
-        isPrime(i);
-    }
+    int count = 0;
     struct timeval start,end;
     gettimeofday(&start, NULL );
 
-
+    #pragma omp parallel for  private(i) schedule(dynamic,20)  num_threads(3) reduction(+:count)
+    for(long i=3; i<=MAX_NUM; i += 2 ) {
+        if(isPrime(i))
+	    count++;
+    }
+    
+    cout<<count<<endl;
     gettimeofday(&end, NULL);
 
     long timeUse =1000000 * ( end.tv_sec - start.tv_sec ) + end.tv_usec - start.tv_usec;
