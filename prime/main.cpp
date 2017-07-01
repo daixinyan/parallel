b369623345c27baf6125bd12521992fc8bfc8365
daixinyan <381675152@qq.com>
2017-07-01 22:36:27 +0800

    submit code of parallel prime,(openmp).

--- prime/main.cpp
+++ prime/main.cpp
@@ -8,39 +8,21 @@
 
 using namespace std;
 
-static int threadNum = 1;
-static long MAX_NUM = 1000 * 1000  ;
+//static int threadNum = 1;
+static long MAX_NUM = 10 * 1000  ;
 
-
-void pthread_execute(void* p) {
-    int num = threadNum;
-    int threadIndex = (long)p;
-
-    for(long i=threadIndex*2+3; i<=MAX_NUM; i += num*2 ) {
-        isPrime(i);
-    }
-}
 
 int main(int argc, char ** args) {
 
-    if(argc>1) {
-        threadNum = atoi(args[1]);
-    }
-    if(argc>2) {
-        MAX_NUM = atol(args[2]);
-    }
 
+    int i;
+    #pragma omp parallel for  private(i) schedule(static,1)  num_threads(parameters.number_of_threads)
+    for(long i=3; i<=MAX_NUM; i += 2 ) {
+        isPrime(i);
+    }
     struct timeval start,end;
     gettimeofday(&start, NULL );
 
-    pthread_t* pthread_handles = new pthread_t[threadNum];
-
-
-    for (long thread = 0; thread < threadNum ; ++thread) {
-        pthread_create(&pthread_handles[thread], NULL,(void* (*)(void*))pthread_execute, (void*)thread);
-    }
-    for(long thread=0; thread<threadNum; thread++)
-        pthread_join(pthread_handles[thread], NULL);
 
     gettimeofday(&end, NULL);
 
---
+++ prime/pthread.cpp
@@ -0,0 +1,54 @@
+//
+// Created by darxan on 2017/6/27.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "prime.h"
+#include <time.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+using namespace std;
+
+static int threadNum = 1;
+static long MAX_NUM = 1000 * 1000  ;
+
+
+void pthread_execute(void* p) {
+    int num = threadNum;
+    int threadIndex = (long)p;
+
+    for(long i=threadIndex*2+3; i<=MAX_NUM; i += num*2 ) {
+        isPrime(i);
+    }
+}
+
+int main(int argc, char ** args) {
+
+    if(argc>1) {
+        threadNum = atoi(args[1]);
+    }
+    if(argc>2) {
+        MAX_NUM = atol(args[2]);
+    }
+
+    struct timeval start,end;
+    gettimeofday(&start, NULL );
+
+    pthread_t* pthread_handles = new pthread_t[threadNum];
+
+
+    for (long thread = 0; thread < threadNum ; ++thread) {
+        pthread_create(&pthread_handles[thread], NULL,(void* (*)(void*))pthread_execute, (void*)thread);
+    }
+    for(long thread=0; thread<threadNum; thread++)
+        pthread_join(pthread_handles[thread], NULL);
+
+    gettimeofday(&end, NULL);
+
+    long timeUse =1000000 * ( end.tv_sec - start.tv_sec ) + end.tv_usec - start.tv_usec;
+    cout << timeUse << endl;
+    return 0;
+}
